a1a9ec30eaebd4b76583a60fac7265061adf2fbc
bobsaladev <89103883+bobsaladev@users.noreply.github.com>
2021-12-30 18:09:03 -0600

    Add files via upload
    Inicio del proyecto - NO ENTREGA

---
+++ script/main-scripts.js
@@ -0,0 +1,7 @@
+function configCanvas(){
+    let canvasWidth = prompt("Enter the canvas Width");
+    let canvasHeight = prompt("Enter the canvas Height");
+    alert("El ancho del canvas es: " + canvasWidth + "px y el alto es: " + canvasHeight + "px");
+}
+
+configCanvas();
